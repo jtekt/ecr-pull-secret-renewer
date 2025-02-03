1daef558ba7c24f6f5e97f3e9ffb41c933f0958a
Maxime Moreillon <m.moreillon@gmail.com>
2025-02-04 07:59:53 +0900

    improved how exceptions are checked

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 SECRET_NAME='ecr-credentials'
-EXCEPTIONS=('kube-system' 'ingress' 'kube-node-lease')
+EXCEPTIONS=('kube-system' 'ingress' 'kube-node-lease' 'public')
 
 echo "Retrieving password from $AWS_ECR_URL"
 PASSWORD=$(aws ecr get-login-password)
@@ -15,8 +15,20 @@ NAMESPACES=$(kubectl get ns --no-headers |  awk '{print $1}')
 
 for NAMESPACE in $NAMESPACES;
 do
-  if [[ ! $(echo ${EXCEPTIONS[@]} | fgrep -w $NAMESPACE ) ]]
+  # Check if namespace is an exception
+  IS_EXCEPTION=false
+  for EXCEPTION in "${EXCEPTIONS[@]}"; do
+    if [[ "$EXCEPTION" == "$NAMESPACE" ]]; then
+        IS_EXCEPTION=true
+        break
+    fi
+  done
+
+  if $IS_EXCEPTION;
   then
+    echo "$NAMESPACE is an exception, skipping..."
+
+  else 
     echo "Applying secret for namesapce $NAMESPACE"
     kubectl create secret docker-registry $SECRET_NAME \
       --docker-server=$AWS_ECR_URL \
@@ -24,8 +36,6 @@ do
       --docker-password $PASSWORD \
       --dry-run=client -o yaml \
       | kubectl apply -n $NAMESPACE -f - 
-  else 
-    echo "$NAMESPACE is an exception, skipping..."
   fi
 done
 
